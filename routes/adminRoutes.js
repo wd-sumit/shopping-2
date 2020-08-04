0fdcd63786ca32069301eee6ead352f79f0a516a
wd-sumit <wdsumit.pal@gmail.com>
2020-08-04 20:14:05 +0530

    routes changed

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/signup',authController.protect(Admin) ,authController.signup(Admin));
 router.post('/login', authController.login(Admin));
-router.post('/forgotpassword', authController.forgotPassword(Admin));
+router.post('/forgotpassword', authController.forgotPassword(Admin, 'admins'));
 router.patch('/resetpassword/:token', authController.resetPassword(Admin));
 router.patch('/updatepassword', authController.protect(Admin), authController.updatePassword(Admin)); 
 router.patch('/deactivateme', authController.protect(Admin), authController.deactivateMe(Admin));
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,16 +6,15 @@ const productRouter = require('./productRoutes');
 
 const router = express.Router();
 
+// Middleware for product by user router mounting
 router.use('/products', authController.protect(User), productRouter);
 
 router.post('/signup', authController.signup(User));
 router.post('/login', authController.login(User));
-router.post('/forgotpassword', authController.forgotPassword(User));
+router.post('/forgotpassword', authController.forgotPassword(User, 'users'));
 router.patch('/resetpassword/:token', authController.resetPassword(User));
 router.patch('/updatepassword', authController.protect(User), authController.updatePassword(User)); 
 router.patch('/deactivateme', authController.protect(User), authController.deactivateMe(User));
 router.get('/logout', authController.logout);
-
-
 
 module.exports = router;
